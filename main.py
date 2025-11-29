a3d15e76c40490475422e4c56ef488cae542fe87
NicWell <wellingtonngc12345@gmail.com>
2025-11-29 08:04:51 -0300

    ajuste na main

--- main.py
+++ main.py
@@ -2,4 +2,22 @@ from fastapi import FastAPI
 
 
 app = FastAPI()
-#para rodar nosso sitema, uvicorn main:app -- reload
+
+from order_routes import order_router #importando a rota de pedidos 
+from auth_routes import auth_router #importando a rota de autorizacao
+
+#incluindo as rotas no app
+app.include_router(auth_router)
+app.include_router(order_router)
+
+
+
+# instanciando o app
+#para rodar nosso sitema, uvicorn main:app --reload
+#endpoints == rotas
+#requisições
+# get > leitura
+# post > enviar
+# patch > editar
+# delete > deletar
+# Temos uma rest Api
